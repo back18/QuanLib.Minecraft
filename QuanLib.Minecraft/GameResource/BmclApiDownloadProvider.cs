ecaaf7a32f31282ec84429d593823ce2a5e07235
Quan <2200191123@qq.com>
2023-10-03 05:04:41 +0800

    修复了部分URL无法重定向到BMCLAPI下载源的bug

--- QuanLib.Minecraft/GameResource/BmclApiDownloadProvider.cs
+++ QuanLib.Minecraft/GameResource/BmclApiDownloadProvider.cs
@@ -10,12 +10,14 @@ namespace QuanLib.Minecraft.GameResource
     {
         public const string ROOT_URL = "https://bmclapi2.bangbang93.com/";
 
-        public const string RESOURCES_DOWNLOAD_URL = ROOT_URL + "assets/";
+        public const string RESOURCES_URL = ROOT_URL + "assets/";
+
+        public const string LIBRARIES_URL = ROOT_URL + "libraries/";
 
         public BmclApiDownloadProvider()
         {
             VersionListUrl = ROOT_URL + "mc/game/version_manifest.json";
-            AssetBaseUrl = RESOURCES_DOWNLOAD_URL;
+            AssetBaseUrl = RESOURCES_URL;
         }
 
         public override string VersionListUrl { get; }
@@ -25,13 +27,29 @@ namespace QuanLib.Minecraft.GameResource
         public override string RedirectUrl(string url)
         {
             if (string.IsNullOrEmpty(url))
+            {
                 return url;
+            }
+            else if (url.StartsWith(MojangDownloadProvider.RESOURCES_URL))
+            {
+                return url.Replace(MojangDownloadProvider.RESOURCES_URL, RESOURCES_URL);
+            }
+            else if (url.StartsWith(MojangDownloadProvider.LIBRARIES_URL))
+            {
+                return url.Replace(MojangDownloadProvider.LIBRARIES_URL, LIBRARIES_URL);
+            }
             else if (url.StartsWith(MojangDownloadProvider.PISTON_DATA_URL))
+            {
                 return url.Replace(MojangDownloadProvider.PISTON_DATA_URL, ROOT_URL);
-            else if (url.StartsWith(MojangDownloadProvider.RESOURCES_DOWNLOAD_URL))
-                return url.Replace(MojangDownloadProvider.RESOURCES_DOWNLOAD_URL, RESOURCES_DOWNLOAD_URL);
+            }
+            else if (url.StartsWith(MojangDownloadProvider.PISTON_META_URL))
+            {
+                return url.Replace(MojangDownloadProvider.PISTON_META_URL, ROOT_URL);
+            }
             else
+            {
                 return url;
+            }
         }
     }
 }
--- QuanLib.Minecraft/GameResource/MojangDownloadProvider.cs
+++ QuanLib.Minecraft/GameResource/MojangDownloadProvider.cs
@@ -8,18 +8,18 @@ namespace QuanLib.Minecraft.GameResource
 {
     public class MojangDownloadProvider : DownloadProvider
     {
-        public const string PISTON_META_URL = "https://piston-meta.mojang.com/";
-
-        public const string PISTON_DATA_URL = "https://piston-data.mojang.com/";
-
-        public const string RESOURCES_DOWNLOAD_URL = "https://resources.download.minecraft.net/";
+        public const string RESOURCES_URL = "https://resources.download.minecraft.net/";
 
         public const string LIBRARIES_URL = "https://libraries.minecraft.net/";
 
+        public const string PISTON_DATA_URL = "https://piston-data.mojang.com/";
+
+        public const string PISTON_META_URL = "https://piston-meta.mojang.com/";
+
         public MojangDownloadProvider()
         {
-            VersionListUrl = PISTON_META_URL + "/mc/game/version_manifest.json";
-            AssetBaseUrl = RESOURCES_DOWNLOAD_URL;
+            VersionListUrl = PISTON_META_URL + "mc/game/version_manifest.json";
+            AssetBaseUrl = RESOURCES_URL;
         }
 
         public override string VersionListUrl { get; }
