ba668bceef386f1d61d8e1ef02c90c4c6b4e6e59
Quan <2200191123@qq.com>
2023-10-14 14:44:33 +0800

    新增QuanLib.Minecraft.RCON模块，用于开发高性能高并发RCON客户端

---
+++ QuanLib.Minecraft.RCON/RconClient.cs
@@ -0,0 +1,27 @@
+﻿using log4net.Core;
+using QuanLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Minecraft.RCON
+{
+    public class RconClient : UnmanagedRunnable
+    {
+        public RconClient(Func<Type, LogImpl> logger) : base(logger)
+        {
+        }
+
+        protected override void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DisposeUnmanaged()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
