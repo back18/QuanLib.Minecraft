b67282db6e6f85233ad9a4d8aef7ab5fd1acf43c
Quan <2200191123@qq.com>
2024-01-14 16:44:12 +0800

    将  CommandBase<TResult> 类转移到单独的.cs文件

---
+++ QuanLib.Minecraft.Command/Models/CommandBase(TResult).cs
@@ -0,0 +1,30 @@
+﻿using QuanLib.Minecraft.Command.Senders;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLib.Minecraft.Command.Models
+{
+    public abstract class CommandBase<TResult> : CommandBase
+    {
+        public virtual bool TrySendCommand(CommandSender sender, object[] inargs, [MaybeNullWhen(false)] out TResult result)
+        {
+            if (!base.TrySendCommand(sender, inargs, out var outargs))
+                goto fail;
+
+            if (!TryParseResult(outargs, out result))
+                goto fail;
+
+            return true;
+
+            fail:
+            result = default;
+            return false;
+        }
+
+        public abstract bool TryParseResult(string[] outargs, [MaybeNullWhen(false)] out TResult result);
+    }
+}
--- QuanLib.Minecraft.Command/Models/CommandBase.cs
+++ QuanLib.Minecraft.Command/Models/CommandBase.cs
@@ -33,24 +33,4 @@ namespace QuanLib.Minecraft.Command.Models
             return false;
         }
     }
-
-    public abstract class CommandBase<TResult> : CommandBase
-    {
-        public virtual bool TrySendCommand(CommandSender sender, object[] inargs, [MaybeNullWhen(false)] out TResult result)
-        {
-            if (!base.TrySendCommand(sender, inargs, out var outargs))
-                goto fail;
-
-            if (!TryParseResult(outargs, out result))
-                goto fail;
-
-            return true;
-
-            fail:
-            result = default;
-            return false;
-        }
-
-        public abstract bool TryParseResult(string[] outargs, [MaybeNullWhen(false)] out TResult result);
-    }
 }
